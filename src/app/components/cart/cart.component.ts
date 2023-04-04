c1a493618fcdf7e580689cedc7aa2af83a18c338
VladanStar <vladancupric@gmail.com>
2023-04-04 20:56:50 +0200

    Add

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/model/product';
 import { CartService } from 'src/app/services/cart.service';
 import { CurrencyPipe } from '@angular/common';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,9 +11,16 @@ import { CurrencyPipe } from '@angular/common';
 })
 export class CartComponent  implements OnInit{
 public items:Product[]=[];
+  public checkoutForm!: FormGroup;
 
-constructor( private cartService:CartService){
-this.items=this.cartService.getItems()
+constructor( private cartService:CartService,
+  private formBuilder:FormBuilder){
+this.items=this.cartService.getItems();
+this.checkoutForm=this.formBuilder.group({
+name:[],
+adderss:[],
+email:[]
+})
 }
 
 ngOnInit(): void {
