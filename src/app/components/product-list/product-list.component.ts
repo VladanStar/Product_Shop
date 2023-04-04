4da903aee50e68ae6ed46eb7006d2863894a3089
VladanStar <vladancupric@gmail.com>
2023-04-04 07:42:13 +0200

    Add

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products:[]=[];
+
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+}
