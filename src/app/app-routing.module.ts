df98475c0fb3cb1cbe605fb400faa651b7bdab60
VladanStar <vladancupric@gmail.com>
2023-04-04 22:59:40 +0200

    Add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductInfoComponent } from './components/product-info/product-info.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 
 const routes: Routes = [
 {path:"", component:ProductListComponent},
 {path:'checkout', component:CartComponent},
 {path:"product/:id", component:ProductInfoComponent},
+{path:"product/add",component:AddProductComponent},
 
 
 ];
