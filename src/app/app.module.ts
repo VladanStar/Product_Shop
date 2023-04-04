c34eae6a1f76a088893e54b9d4a33f84c43af8c2
VladanStar <vladancupric@gmail.com>
2023-04-04 18:21:55 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import {AngularFireModule} from "@angular/fire/compat"
+import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from '../environments/environment';
 import { ProductInfoComponent } from './components/product-info/product-info.component';
 import { CartComponent } from './components/cart/cart.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SumPipe } from './pipes/sum.pipe';
-
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,15 +21,15 @@ import { SumPipe } from './pipes/sum.pipe';
     CartComponent,
     HeaderComponent,
     SumPipe,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase)
-
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
