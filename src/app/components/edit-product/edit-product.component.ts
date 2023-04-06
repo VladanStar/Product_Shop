2307ea1e0338de6aebc653e4ff62466be12fc402
VladanStar <vladancupric@gmail.com>
2023-04-06 15:11:18 +0200

    Add

--- src/app/components/edit-product/edit-product.component.ts
+++ src/app/components/edit-product/edit-product.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { update } from '@angular/fire/database';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/model/product';
+import { ProductService } from 'src/app/services/servis.service';
+
 
 @Component({
   selector: 'app-edit-product',
@@ -6,15 +12,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-product.component.css']
 })
 export class EditProductComponent implements OnInit {
-constructor(){
+
+
+  id:any;
+  product:Product = {
+    name: '',
+    productId: "",
+    descr: '',
+    price: 0,
+
+};
+constructor(private productService:ProductService,
+  private route:ActivatedRoute,
+  private router:Router){
 
 }
 ngOnInit(): void {
+  this.id= this.route.snapshot.paramMap.get("id")
+   if(this.id){
 
+
+   this.productService.get(this.id).subscribe(p => {
+    this.product = p;
+    console.log(this.product);
+  });
+}
 }
 
-
-editPhone(f:any){
-
+editPhone(f:NgForm){
+  // console.log(f.value)
+   let id =this.id as string;
+  this.productService.update(id, f.value);
+  window.alert('Phone is Edited')
+  this.router.navigate(["/"])
+  // f.reset();
+    }
 }
-}
+
